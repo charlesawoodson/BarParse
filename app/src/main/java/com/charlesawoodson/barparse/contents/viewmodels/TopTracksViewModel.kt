b8bf0729e014dd60c0b29ffa35c651ccfdefc446
Charles Woodson <CharlesAWoodson@gmail.com>
2021-03-09 16:14:33 -0500

    Fix to track layout padding and removed unused items in TopTracksViewModel

--- app/src/main/java/com/charlesawoodson/barparse/contents/viewmodels/TopTracksViewModel.kt
+++ app/src/main/java/com/charlesawoodson/barparse/contents/viewmodels/TopTracksViewModel.kt
@@ -1,20 +1,17 @@
 package com.charlesawoodson.barparse.contents.viewmodels
 
-import androidx.hilt.Assisted
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.charlesawoodson.barparse.contents.bases.BaseViewModel
-import com.charlesawoodson.barparse.contents.responses.Track
 import com.charlesawoodson.barparse.contents.repositories.MusixMatchRepository
+import com.charlesawoodson.barparse.contents.responses.Track
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class TopTracksViewModel @Inject constructor(
-    @Assisted private val savedStateHandle: SavedStateHandle,
     private val musicRepository: MusixMatchRepository
 ) : BaseViewModel() {
 
